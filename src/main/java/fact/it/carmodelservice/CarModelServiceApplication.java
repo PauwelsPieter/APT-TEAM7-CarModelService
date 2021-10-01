3af894ed3493a66bb47271ef6e812fd0030e7c44
Pieter Pauwels <r0796064@student.thomasmore.be>
2021-10-01 11:18:09 +0200

    Initial commit

---
+++ src/main/java/fact/it/carmodelservice/CarModelServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.carmodelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarModelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarModelServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/carmodelservice/CarModelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.carmodelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarModelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
